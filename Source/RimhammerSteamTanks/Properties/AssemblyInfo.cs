337bdf9a2b8c32fdf47663898df0a5f58b9f21e3
Johnny Hastings <johnny.hastings@gmail.com>
2024-01-06 10:55:39 -0600

    Initial commit

---
+++ Source/RimhammerSteamTanks/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+//General information about an assembly is controlled by the following attributes.
+//Modify these attributes ​​to change the information associated with an assembly.
+[assembly: AssemblyTitle("Rimhammer Steam Tanks")]
+[assembly: AssemblyDescription("Adds Warhammer themed Steam Tanks to RiwWorld.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Johnny Hastings")]
+[assembly: AssemblyProduct("RimhammerSteamTanks")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// Setting ComVisible to FALSE makes the types in this assembly invisible to COM components.
+// If you need to access a type in this assembly from COM, you should set the ComVisible attribute for this type to True.
+[assembly: ComVisible(false)]
+
+// This GUID determines the ID of the type library when this project is exposed to COM
+[assembly: Guid("fec83340-7a62-4ebc-9487-9fd715b1c058")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      major version.minor version.build number.revision
+//
+// You can specify any values ​​or use default values ​​for the build and revision numbers by typing "*" as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/RimhammerSteamTanks/SteamBlastProjectile.cs
@@ -0,0 +1,41 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using VFECore;
+
+namespace RimhammerSteamTanks
+{
+    class SteamBlastProjectile : ExpandableProjectile
+    {
+		public override void DoDamage(IntVec3 pos)
+		{
+			base.DoDamage(pos);
+			try
+			{
+				if (pos != this.launcher.Position && this.launcher.Map != null && GenGrid.InBounds(pos, this.launcher.Map))
+				{
+					var list = this.launcher.Map.thingGrid.ThingsListAt(pos);
+					for (int num = list.Count - 1; num >= 0; num--)
+					{
+						if (IsDamagable(list[num]))
+						{
+							this.customImpact = true;
+							base.Impact(list[num]);
+							this.customImpact = false;
+						}
+					}
+				}
+			}
+			catch { };
+		}
+
+		public override bool IsDamagable(Thing t)
+		{
+			return base.IsDamagable(t) && t.def != ThingDefOf.Fire;
+		}
+	}
+}
